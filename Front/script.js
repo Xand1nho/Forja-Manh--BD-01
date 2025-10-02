20336e06b68171fea42dd73ed8d9c44856dba16b
Xandinho <souzasantosalexandre825@gmail.com>
2025-10-02 11:30:18 -0300

    Add files via upload

--- Front/script.js
+++ Front/script.js
@@ -1,3 +1,35 @@
 const resultsDiv = document.getElementById("results")
 
-console.log(resultsDiv)
+async function fetchFruits(){ 
+    resultsDiv.innerHTML = "<p>Carregando...</p>"
+
+    try {
+        const response = await fetch('../Dados/data.json')
+        const data = await response.json()
+
+        if(data.error){
+            resultsDiv.innerHTML = "<p> Erro ao buscar!</p>"
+            return
+        }
+        resultsDiv.innerHTML = ''
+
+        data.forEach(fruits => {
+            const card = document.createElement("div")
+            card.className = "card"
+            card.innerHTML = `
+                <img src="${fruits.imagem}" alt="${fruits.nome}"
+                <h2>${fruits.nome}</h2>
+                <h3>${fruits.descricao}</h3>
+                <p>R$${fruits.preco.toFixed(2)}</p>
+            `
+            resultsDiv.appendChild(card)
+            
+            });
+
+    } catch (error) {
+        resultsDiv.innerHTML = "<p>Erro ao buscar produtos!</p>"
+    }
+}
+        
+
+fetchFruits()
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Forja-Manh--BD-01
 Consumo de JSON
+
+
+As Tags de html são exemplos como: body, p, div, h1, h2, img, br
+
+     atributo
+<div class = "nome" id= ""
+     estilo (css) 
+
+width é largura
+Css propriedade: color widht 100 px
+
+Tudo que vem depois da tag são atributos
